a02db50d43191a6c57f502726bd1f8012ffe7f61
Lizandra Anjos <138258723+sisedusiqueira@users.noreply.github.com>
2023-07-18 17:57:30 -0300

    Update 1MiniTeste.py
    Atualização no código do Miniteste3

--- programacao_mod1/miniteste3/1MiniTeste.py
+++ programacao_mod1/miniteste3/1MiniTeste.py
@@ -1,11 +1,3 @@
-pe_direito_chip_identificacao = 4.0
-pe_esquero_tipo_alimento = 3.5
-
-print('='*50)
-
-qtd_frango = (float(input('Digite o número de frangos: ')))
-
-total = (pe_direito_chip_identificacao + ( 2 * pe_esquero_tipo_alimento))
-resultado = total * qtd_frango
-
-print('Quantidade total de gastos é: ', resultado)
+'''
+Comandos
+'''
